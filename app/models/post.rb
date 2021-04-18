77c1f55955525076078c5c40a91ff2bb61e223f0
Avery Reichler <dreichle@uccs.edu>
2021-04-18 02:34:40 +0000

    Reset bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ApplicationRecord
-
     extend FriendlyID
     friendly_id :title, use: :slugged
 
     def should_generate_new_friendly_id?
         title_changed?
     end
-
+    
 end
