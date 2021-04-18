b6417d95337956702bdd331652a0c78b9925f320
Avery Reichler <dreichle@uccs.edu>
2021-04-18 04:00:39 +0000

    Finished I3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :description, :slug)
+      params.require(:post).permit(:title, :body, :description)
     end
 end
