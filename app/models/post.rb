6e65995ae3788433e147703528aea1772d4c2973
Avery Reichler <dreichle@uccs.edu>
2021-04-25 00:07:01 +0000

    Added RSpec and SimpleCov, reached 70% coverage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
     extend FriendlyId
     friendly_id :title, use: :slugged
 
+    validates_presence_of :title, :description
+
     def should_generate_new_friendly_id?
         title_changed?
     end
