11faa80c824fd1b0b0136bc9d2afdecfcfa161ed
Avery Reichler <dreichle@uccs.edu>
2021-05-01 18:52:59 +0000

    Fixed routes, updated footer text

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   
   #root to: 'home#posts'
-  root to: 'posts#index'
+  root to: 'posts#index' #best default route
   #root :to => "pages#home"
+
+  #authenticated :user do
+  #  root 'posts#index', as: :authenticated_root
+  #end
+
+  #root "home#index"
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
