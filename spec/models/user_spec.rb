40a50cc21ab9dad292689435c23505b0e20a3941
Avery Reichler <dreichle@uccs.edu>
2021-05-09 05:35:46 +0000

    Improved testing to 23 examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation texts" do
+    it "ensures the email is present" do
+      user = User.new(password: "securepassword123")
+      expect(user.valid?).to eq(false)
+    end
+
+    it "ensures the password is present" do
+      user = User.new(email: "name@email.com")
+      expect(user.valid?).to eq(false)
+    end
+
+    it "should allow a signup" do
+      user = User.new(email: "name@email.com", password: "securepassword123")
+      expect(user.valid?).to eq(true)
+    end
+  end
 end
